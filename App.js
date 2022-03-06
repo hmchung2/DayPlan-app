71284e81308d5a12eaf9722a7cb9cca232084f26
hmchung2 <hmchung2@gmail.com>
2022-03-07 01:49:23 +0900

    event

--- App.js
+++ App.js
@@ -1,11 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Pressable,
+  TextInput,
+} from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState();
+
+  const onChangeText = (payload) => {
+    console.log(payload);
+    setText(payload);
+  };
+
+  const travel = () => {
+    setWorking(false);
+    console.log("travel");
+  };
+
+  const work = () => {
+    setWorking(true);
+    console.log("work");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableHighlight
+          underlayColor="grey"
+          activeOpacity={0.01}
+          onPress={travel}
+        >
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableHighlight>
+
+        {/* <Pressable>
+          <Text style={styles.btnText}>Travel</Text>
+        </Pressable> */}
+        {/* <TouchableOpacity activeOpacity={0}>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableOpacity> */}
+      </View>
+
+      <TextInput
+        onChangeText={onChangeText}
+        returnKeyType="send"
+        //keyboardType="default"
+        style={styles.input}
+        placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+      />
     </View>
   );
 }
@@ -13,8 +82,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
