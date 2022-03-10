3903a3b253652be78d72cf97b36184ea76a459a8
hmchung2 <hmchung2@gmail.com>
2022-03-10 17:54:46 +0900

    publish

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
 import {
   StyleSheet,
   Text,
@@ -10,16 +11,20 @@ import {
   Pressable,
   TextInput,
   ScrollView,
+  Alert,
+  BackHandler,
 } from "react-native";
 import { theme } from "./colors";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
+import { Fontisto } from "@expo/vector-icons";
 const STORAGE_KEY = "@toDos";
+const STATUS_KEY = "@status";
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({});
+  const [editingKey, setEditingKey] = useState("");
 
   const saveToDos = async (toSave) => {
     try {
@@ -35,13 +40,29 @@ export default function App() {
     console.log("changed : " + text);
   };
 
+  const saveStatus = async (status) => {
+    try {
+      console.log("Saving Working : " + status);
+      const s = JSON.stringify(status);
+      await AsyncStorage.setItem(STATUS_KEY, s);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const travel = () => {
+    cancelEdit();
+    setEditingKey("");
     setWorking(false);
+    saveStatus(false);
     console.log("travel");
   };
 
   const work = () => {
+    cancelEdit();
+    setEditingKey("");
     setWorking(true);
+    saveStatus(true);
     console.log("work");
   };
 
@@ -50,6 +71,29 @@ export default function App() {
     console.log(s);
 
     setToDos(JSON.parse(s));
+  };
+
+  const loadStatus = async () => {
+    console.log("loading Status");
+    const s = await AsyncStorage.getItem(STATUS_KEY);
+    console.log(s);
+    setWorking(JSON.parse(s));
+  };
+
+  const editTodo = async (key) => {
+    if (text === "") {
+      return;
+    }
+
+    const editingTodo = toDos[key];
+    console.log(editingTodo);
+    editingTodo["text"] = text;
+    const newTodos = toDos;
+    newTodos[key] = editingTodo;
+    setToDos(newTodos);
+    await saveToDos(newTodos);
+    setText("");
+    setEditingKey("");
   };
 
   const addTodo = async () => {
@@ -62,7 +106,7 @@ export default function App() {
     // });
     const newToDos = {
       ...toDos,
-      [Date.now()]: { text, work: working },
+      [Date.now()]: { text, work: working, complete: false },
     };
     setToDos(newToDos);
     await saveToDos(newToDos);
@@ -72,9 +116,61 @@ export default function App() {
   const logText = () => {
     console.log("log : " + text);
   };
+
+  const editText = (key) => {
+    setEditingKey(key);
+  };
+
+  const completeTodo = async (key) => {
+    const newTodos = { ...toDos };
+    const completeTodo = newTodos[key];
+    completeTodo["complete"] = true;
+    console.log("completed : ", completeTodo);
+
+    newTodos[key] = completeTodo;
+    setToDos(newTodos);
+    await saveToDos(newTodos);
+  };
+
+  const cancelEdit = () => {
+    setText("");
+    setEditingKey("");
+  };
+
+  const deleteTodo = (key) => {
+    Alert.alert("Delete to Do? ", "Are you sure?", [
+      { text: "Cancel" },
+      {
+        text: "Yes",
+        onPress: async () => {
+          const newTodos = { ...toDos };
+          delete newTodos[key];
+          console.log("deleting");
+          setToDos(newTodos);
+          console.log(toDos);
+          await saveToDos(newTodos);
+        },
+      },
+    ]);
+  };
+
+  const handleBackButtonClick = () => {
+    console.log("back Button Clicked");
+    cancelEdit();
+    return true;
+  };
+
   //console.log(toDos);
   useEffect(() => {
+    BackHandler.addEventListener("hardwareBackPress", handleBackButtonClick);
+    loadStatus();
     loadToDos();
+    return () => {
+      BackHandler.removeEventListener(
+        "hardwareBackPress",
+        handleBackButtonClick
+      );
+    };
   }, []);
 
   return (
@@ -123,13 +219,66 @@ export default function App() {
         value={text}
         keyboardType="default"
         placeholder={working ? "Add a To Do" : "Where do you want to go?"}
-        style={styles.input}
+        style={editingKey == "" ? styles.input : null}
       />
       <ScrollView>
         {Object.keys(toDos).map((key) =>
           working == toDos[key].work ? (
             <View style={styles.toDo} key={key}>
-              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+              {key == editingKey ? (
+                <TextInput
+                  onSubmitEditing={() => editTodo(key)}
+                  onChangeText={onChangeText}
+                  returnKeyType="done"
+                  value={text}
+                  autoFocus={true}
+                  keyboardType="default"
+                  style={styles.editInput}
+                  onEndEditing={cancelEdit}
+                ></TextInput>
+              ) : (
+                <Text
+                  style={
+                    toDos[key].complete
+                      ? {
+                          ...styles.toDoText,
+                          textDecorationLine: "line-through",
+                          color: theme.grey,
+                          fontWeight: "300",
+                        }
+                      : styles.toDoText
+                  }
+                >
+                  {toDos[key].text}
+                </Text>
+              )}
+
+              <View style={styles.emoJibtn}>
+                <TouchableOpacity
+                  style={styles.eachEmoji}
+                  onPress={() => deleteTodo(key)}
+                >
+                  <Fontisto name="trash" size={18} color={theme.grey} />
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={styles.eachEmoji}
+                  onPress={() => editText(key)}
+                >
+                  <AntDesign name="edit" size={18} color="black" />
+                </TouchableOpacity>
+
+                {!toDos[key].complete ? (
+                  <TouchableOpacity
+                    style={styles.eachEmoji}
+                    onPress={() => completeTodo(key)}
+                  >
+                    <Fontisto name="check" size={18} color={theme.grey} />
+                  </TouchableOpacity>
+                ) : (
+                  <TouchableOpacity></TouchableOpacity>
+                )}
+              </View>
             </View>
           ) : null
         )}
@@ -162,15 +311,31 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   toDo: {
+    flex: 1,
     backgroundColor: theme.toDoBg,
     marginBottom: 10,
     paddingVertical: 10,
     paddingHorizontal: 40,
     borderRadius: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   toDoText: {
+    flex: 5,
     color: "white",
     fontSize: 16,
-    fontWeight: "500",
+    fontWeight: "800",
+  },
+  emoJibtn: {
+    flex: 3,
+    flexDirection: "row-reverse",
+    justifyContent: "flex-start",
+  },
+  eachEmoji: {
+    marginHorizontal: 10,
+  },
+  editInput: {
+    backgroundColor: "#D3D3D3",
   },
 });
